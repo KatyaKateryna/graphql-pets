6a7c3585238a6fce183df78b76626236ad195a6f
KatyaKateryna <kateryna.lelych@wework.com>
2021-10-27 16:11:16 +0300

    Generated an empty project with https://start.spring.io/

---
+++ src/main/java/com/softserve/graphqlpets/GraphqlPetsApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.graphqlpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPetsApplication.class, args);
+	}
+
+}
